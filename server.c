b9481812ee42df6120d17b2cbab261ef0a32c196
medameen <MohamedAmin.BenMhamed@esprit.tn>
2025-03-25 06:02:54 +0100

    read-index-content

--- server.c
+++ server.c
@@ -5,20 +5,21 @@
 #include <ws2tcpip.h>
 
 
-#define ROOT_PATH "/root"
-#define INDEX   "index.html"
+#define ROOT_PATH       "root"
+#define INDEX           "index.html"
 
 
 #pragma comment(lib, "ws2_32.lib") // winsock lib
 
-
+// Threading
+// ===================================
 typedef struct 
 {
         SOCKET socket;
         char *buf;
         int len;
         int flags;
-
+        
 } send_params;
 
 
@@ -28,6 +29,7 @@ DWORD WINAPI th_send(LPVOID lpParam)
         send(params->socket, params->buf, params->len, params->flags);
         return 0;
 }
+// ===================================
 
 
 int main(int argc, char **argv)
@@ -61,7 +63,8 @@ int main(int argc, char **argv)
         printf("Socket created.\n");
         //
 
-        //Prepare the sockaddr_in structure
+        // Prepare the sockaddr_in structure
+        //
 	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_family      = AF_INET;
 	server.sin_port        = htons( 8888 );
@@ -80,29 +83,73 @@ int main(int argc, char **argv)
         printf("Bound\n");
 
         // Putting socket to listening mode
-        listen(sock, 3);
+        listen(sock, SOMAXCONN);
 
         printf("awaiting incoming connections ...\n");
 
         int c = sizeof(struct sockaddr_in);
 
-        new_sock = accept(sock, (struct sockaddr *)&client, &c);
+        //new_sock = accept(sock, (struct sockaddr *)&client, &c);
 
         char *client_ip;
         int client_port;
 
-        // Threading
+        // Getting index content
+        // =======================================
+        
+        char index_file_path[256] = ROOT_PATH;
+        strcat(index_file_path, "\\index.html");
+
+        printf("current index path : %s\n", index_file_path);
+
+        //
+        FILE *index_file = fopen(index_file_path, "r");
+
+        if(index_file == NULL)
+        {
+                printf("ERROR CANNOT READ INDEX FILE\n");
+                return 1;
+        }
+
+        
+        char index_content_buffer[4096];
+        size_t bytes_read                = fread(index_content_buffer, 1, sizeof(index_content_buffer) - 1, index_file);
+        index_content_buffer[bytes_read] = '\0'; 
+
+        printf("index file buffer ==============:\n %s", index_content_buffer);
+
+        fclose(index_file);
         //
 
+        char *HTTP_HEADER = "HTTP/1.1 200 OK\r\n"
+                            "Content-Type: text/html; charset=UTF-8\r\n"
+                            "\r\n";
+
+        char message[8192];
+        strcpy(message, HTTP_HEADER);
+        strcat(message, index_content_buffer);
+
+        printf("message content ================ :\n %s", message);
+
+        //======================================
+
+        // Threading
+        //======================================
         HANDLE thread;
         DWORD threadId;
 
-        char *message = "Hello Client , your connection is received\n";
-
         // cannot handle more than one connection at a time.
+        //
         while(new_sock != INVALID_SOCKET)
         {
                 new_sock = accept(sock , (struct sockaddr *)&client, &c);
+
+                if(new_sock == INVALID_SOCKET)
+                {
+                        printf("accept failed : ERROR::%d\n", WSAGetLastError());
+                        WSACleanup();
+                        return 1;
+                }
 
                 printf("connection accepted\n");
 
@@ -124,12 +171,7 @@ int main(int argc, char **argv)
 
         }
 
-        if(new_sock == INVALID_SOCKET)
-        {
-                printf("accept failed : ERROR::%d\n", WSAGetLastError());
-                WSACleanup();
-                return 1;
-        }
+        
 
 
         //WaitForSingleObject(thread, INFINITE);
